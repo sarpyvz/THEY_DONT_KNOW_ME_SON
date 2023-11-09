733df7a3ab483337b47bff62623aad17c5d78869
Sarp Yavuz <147308207+sarpyvz@users.noreply.github.com>
2023-11-09 15:21:25 +0300

    Update davidgoggins.cpp

--- davidgoggins.cpp
+++ davidgoggins.cpp
@@ -8,8 +8,7 @@ int main(){
     int HoursGrined;
     string Bitch_today;
     string Bad_felt;    
-    
-    while ( david >= 80 ){
+
         cout << "Have you carried boats and logs today ?"<<endl;
         cin >> Boats;
         if ( Boats == "yes" || Boats == "Yes"||Boats == "YES"){
@@ -30,16 +29,18 @@ int main(){
         cout << "Have you done anything that made you feel like shit today" << endl;
         cin >> Bad_felt;
         if ( Bad_felt == "yes" ||Bad_felt == "Yes" ||Bad_felt == "YES"){
-            cout << "Go get some LA bu benim söz değil ama GOOD" << endl;
+            cout << "Go get some LA bu benim söz degil ama GOOD" << endl;
             david += 20;
         }
         else{
             cout << "GET BACK TO WORK GET BACK TO FUCKING WORK" << endl;
         }
+       
 
 
-    }
-    cout << "THEY DO NOT KNOW YOU SON NICE BROTHER" << endl;
+    
+    cout << david << "% DAVID GOGGINS YOU ARE"<< endl;
+    cout << "THEY DO NOT KNOW YOU SON " << endl;
     
 
 
